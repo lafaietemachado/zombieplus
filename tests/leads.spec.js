6425362228b2145d24503f860199a74940b6d4be
Lafaiete Machado <lafaiete.machado@gmail.com>
2025-01-23 16:03:44 -0300

    organizando codigo com page objects

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -1,23 +1,56 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+const { LandingPage } = require('./pages/LandingPage')
+
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-
-  await page.getByRole('button', { name: /Aperte o play/ }).click()
-
-   await expect(
-    page.getByTestId('modal').getByRole('heading')
-  ).toHaveText('Fila de espera')
-
-  await page.getByPlaceholder('Seu nome completo').fill('Lafaiete Machado')
-  await page.getByPlaceholder('Seu email principal').fill('lafaiete.machado@gmail.com')
-
-  await page.getByTestId('modal')
-    .getByText('Quero entrar na fila!').click()
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Lafaiete Machado', 'lafaiete.machado@gmail.com')
 
   const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
-  await expect(page.locator('.toast')).toHaveText(message)
+  await landingPage.toastHaveText(message)
 
-  await expect(page.locator('.toast')).toBeHidden({timeout: 5000})
+});
+
+test('não deve cadastrar com email incorreto', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Lafaiete Machado', 'lafaiete.machado.com.br')
+
+  await landingPage.alertHaveText('Email incorreto')
+});
+
+test('não deve cadastrar quando o nome não é preenchido', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('', 'lafaiete.machado@gmail.com')
+
+  await landingPage.alertHaveText('Campo obrigatório')
+});
+
+test('não deve cadastrar quando o email não é preenchido', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('Lafaiete Machado', '')
+
+  await landingPage.alertHaveText('Campo obrigatório')
+});
+
+test('não deve cadastrar quando nenhum campo é preenchido', async ({ page }) => {
+  const landingPage = new LandingPage(page)
+  
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm('', '')
+
+  await landingPage.alertHaveText(['Campo obrigatório', 'Campo obrigatório'])
 });
