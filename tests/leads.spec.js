0cc8d41cb666e3fe874620014f78be9c97f6e3ef
Lafaiete Machado <lafaiete.machado@gmail.com>
2025-01-23 11:13:21 -0300

    limpando comentarios do codigo

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -4,18 +4,9 @@ const { test, expect } = require('@playwright/test');
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
-  // Usando Xpath para buscar o elemento
-  //await page.click('//button[text()="Aperte o play... se tiver coragem"]')
-
-  // Usando tag do HTML com o texto representando o texto
-  //await page.getByRole('button', { name: 'Aperte o play... se tiver coragem' }).click()
-
-  // Usando as / ao invé de ' ele usa a função de contains
   await page.getByRole('button', { name: /Aperte o play/ }).click()
 
-  //validando se após o click aparece o modal de preenchimento, usando o getByTestId pois tem data-testid="modal", o heading serve
-  //para pegar elentos em todos os H1, h2, etc e depois valido pelo texto Fila de espera
-  await expect(
+   await expect(
     page.getByTestId('modal').getByRole('heading')
   ).toHaveText('Fila de espera')
 
