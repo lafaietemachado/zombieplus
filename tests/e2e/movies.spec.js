23438b4fd1640af16bce64b259512d40a843e170
Lafaiete Machado <lafaiete.machado@gmail.com>
2025-01-29 16:35:38 -0300

    Continuação da Seção 4

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,22 +1,17 @@
-const { test } = require('@playwright/test')
+const { test } = require('../support/')
 
-const { LoginPage } = require('../pages/LoginPage')
-const { Toast } = require('../pages/Components')
-const { MoviesPage } = require('../pages/MoviesPage')
-
-let loginPage
-let toast
-let moviesPage
-test.beforeEach(({ page }) => {
-    loginPage = new LoginPage(page)
-    toast = new Toast(page)
-    moviesPage = new MoviesPage(page)
-})
+const data = require('../support/fixtures/movies.json')
+const { executeSQL } = require('../support/database')
 
 test('deve poder cadastrar um novo filme', async ({ page }) => {
-    await loginPage.visit()
-    await loginPage.submit('admin@zombieplus.com', 'pwd123')
-    await moviesPage.isLoggedIn()
+    const movie = data.create
+    await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
 
-    await moviesPage.create('Nome do Filme', 'Sinopse do Filme', 'Empresa', '2023')
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', 'pwd123')
+    await page.movies.isLoggedIn()
+
+    await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year)
+
+    await page.toast.containText('Cadastro realizado com sucesso!')
 })
