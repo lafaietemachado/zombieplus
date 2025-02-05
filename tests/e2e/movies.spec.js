83a99ceb8b5d3c26e4c00d0b799651206ab28f10
Lafaiete Machado <lafaiete.machado@gmail.com>
2025-02-05 15:33:47 -0300

    Finalizando seção 4

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,5 +1,4 @@
 const { test } = require('../support/')
-
 const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
 
@@ -7,11 +6,22 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
     const movie = data.create
     await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
 
-    await page.login.visit()
-    await page.login.submit('admin@zombieplus.com', 'pwd123')
-    await page.movies.isLoggedIn()
-
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
     await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year)
-
+    await page.movies.submit()
     await page.toast.containText('Cadastro realizado com sucesso!')
+})
+
+test('não deve cadastrar quando os campos obrigatórios não são preenchidos', async ({page}) => {
+    
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.goForm()
+    await page.movies.submit()
+
+    await page.movies.alertHaveText([
+        'Por favor, informe o título.',
+        'Por favor, informe a sinopse.',
+        'Por favor, informe a empresa distribuidora.',
+        'Por favor, informe o ano de lançamento.'
+    ])
 })
